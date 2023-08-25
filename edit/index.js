9e602020d00c2941f6bd8bac0b5da2b59cbcd0ba
Rerebrace <125512517+Rerebrace@users.noreply.github.com>
2023-08-24 20:08:18 -0400

    New functino

--- edit/index.js
+++ edit/index.js
@@ -2,21 +2,27 @@ let titleP = document.getElementById("titlePreview");
 let articleP = document.getElementById("articlePreview");
 let input1 = document.getElementById("titleText");
 let input2 = document.getElementById("articleText");
+let password = document.getElementById("password");
+let tableName = "News Articles";
 
 
 function previewText(inputType) {
-  let previewType = document.getElementById(inputType);
+    let previewType = document.getElementById(inputType);
   
-  if(inputType == "titlePreview") {
-    previewType.innerHTML = titleText.value;
-  }
+    if(inputType == "titlePreview") {
+        previewType.innerHTML = titleText.value;
+    }
 
-  if(inputType == "articlePreview") {
-    previewType.innerHTML = articleText.value;
-  }
+    if(inputType == "articlePreview") {
+        previewType.innerHTML = articleText.value;
+    }
 }
 
-function enterText() {
-
+async function enterText() {
+    if(password.value == "1234") {
+        let time = ((new Date()).toISOString()).toLocaleString('zh-TW');
+        const { count, error } = await _supabase.from(tableName).select('*', { count: 'exact', head: true });
+        const { error1 } = await _supabase.from(tableName).insert({ id: count+1, title: titleText.value, contents: articleText.value, date:time});
+    }
 }
 
