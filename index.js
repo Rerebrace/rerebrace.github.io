d5c3ac522f57fc1411bcdb21abcf1c8eb141f034
Rerebrace <125512517+Rerebrace@users.noreply.github.com>
2023-08-25 10:34:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 let articleHolder = document.getElementById("articleHolder");
 let tableName = "News Articles";
 let contents = "";
-
+let titleVar = "";
+let textVar = "";
+let dateVar = "";
 
 
 async function retrieveArticles() {
-  
+    
     const { data, error } = await _supabase.from(tableName).select();
     const { count, error1 } = await _supabase.from(tableName).select('*', { count: 'exact', head: true });
     let currentNumber = count;
-    
-    for(let i = 0; i < 2; i++) {
-        for(let j = 0; j < currentNumber; j++) {
+    let repeatCount = count;
+
+    if(repeatCount > 11) {
+        repeatCount = 10;
+    }
+
+    console.log(data);
+    for(let i = 0; i < repeatCount; i++) {
+        for(let j = 0; j < count; j++) {
             if(data[j]["id"] == currentNumber) {
-                contents += "<div class="column2"><div class="grouping"><h3 class="plaintext">${data[j]["title"]}: ${data[j]["date"]}</h3><p class = "plaintext">${data[j]["contents"]}</p></div></div>";
-                currentNumber--;  
+                titleVar = data[j]["title"];
+                textVar = data[j]["contents"];
+                dateVar = data[j]["date"];
+                contents += '<div class="column2"><div class="grouping"><h3 class="plaintext">' + titleVar + ': ' + dateVar + '</h3><p class = "plaintext">' + textVar + '</p></div></div>';
+                console.log(contents);
+                currentNumber-=1;
+                articleHolder.innerHTML = contents;
+                break;
             }
         }
-    }
-    articleHolder.innerHTML = contents;
-
+    }    
 }
 
 retrieveArticles();
