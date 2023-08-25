e1fb00c80b3961f2e43f81fa8a8ffa626c453d9c
Rerebrace <125512517+Rerebrace@users.noreply.github.com>
2023-08-24 22:06:55 -0400

    articles retrieved

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+let articleHolder = document.getElementById("articleHolder");
+let tableName = "Download Numbers";
 
+
+
+async function retrieveArticles() {
+  
+    const { data, error } = await _supabase.from(tableName).select();
+    const { count, error1 } = await _supabase.from(tableName).select('*', { count: 'exact', head: true });
+    let currentNumber = count;
+    
+    for(let i = 0; i < 10; i++) {
+        for(let j = 0; j < 3; j++) {
+            if(data[j]["id"] == currentNumber) {
+                articleHolder.innerHTML += <div class="column2"><div class="grouping"><h3 class="plaintext">${data[j]["title"]}: ${data[j]["date"]}</h3><p class = "plaintext">${data[j]["contents"]}</p></div></div>;
+                currentNumber--;  
+            }
+        }
+    }
+}
+
+retrieveArticles();
