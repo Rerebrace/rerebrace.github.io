7a0bceffffa9bd0fa1318d7c33a8b5b3593b4302
Rerebrace <125512517+Rerebrace@users.noreply.github.com>
2023-08-26 09:15:08 -0400

    Update index.js

--- edit/index.js
+++ edit/index.js
@@ -23,7 +23,7 @@ async function enterText() {
     
     const { data, error } = await _supabase.auth.signInWithPassword({email: yourEmail, password: yourPassword,});
     let time = ((new Date()).toISOString()).toLocaleString('zh-TW');
-    const { count, error } = await _supabase.from(tableName).select('*', { count: 'exact', head: true });
-    const { error1 } = await _supabase.from(tableName).insert({ id: count+1, title: titleText.value, contents: articleText.value, date:time});
+    const { count, error1 } = await _supabase.from(tableName).select('*', { count: 'exact', head: true });
+    const { error2 } = await _supabase.from(tableName).insert({ id: count+1, title: titleText.value, contents: articleText.value, date:time});
 }
 
