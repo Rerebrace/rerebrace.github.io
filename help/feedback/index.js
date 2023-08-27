ca13235d68a3d375f1708a28779f5f19e1699dd3
Rerebrace <125512517+Rerebrace@users.noreply.github.com>
2023-08-26 21:30:45 -0400

    Update index.js

--- help/feedback/index.js
+++ help/feedback/index.js
@@ -1,1 +1,11 @@
+let feedbackName = document.getElementById("name");
+let feedbackText = document.getElementById("feedback");
+let tableName = "Feedback";
 
+async function enterText() {
+    console.log(name.value);
+    console.log(feedback.value);
+
+    const { count, error } = await _supabase.from(tableName).select('*', { count: 'exact', head: true });
+    const { error1 } = await _supabase.from(tableName).insert({ id: count+1, name: feedbackName.value, contents: feedbackText.value});
+}
