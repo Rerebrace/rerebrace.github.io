579973cf3beac9a674ef3e26d4a677a44b4fb76e
Rerebrace <125512517+Rerebrace@users.noreply.github.com>
2023-08-26 21:49:43 -0400

    Update index.js

--- help/feedback/index.js
+++ help/feedback/index.js
@@ -1,11 +1,39 @@
 let feedbackName = document.getElementById("name");
 let feedbackText = document.getElementById("feedback");
 let tableName = "Feedback";
+let nameVar = "";
+let textVar = "";
 
 async function enterText() {
-    console.log(name.value);
-    console.log(feedback.value);
+    console.log(feedbackName.value);
+    console.log(feedbackText.value);
 
     const { count, error } = await _supabase.from(tableName).select('*', { count: 'exact', head: true });
     const { error1 } = await _supabase.from(tableName).insert({ id: count+1, name: feedbackName.value, contents: feedbackText.value});
 }
+
+
+
+async function retrieveFeedbackLogs() {
+    
+    const { data, error } = await _supabase.from(tableName).select();
+    const { count, error1 } = await _supabase.from(tableName).select('*', { count: 'exact', head: true });
+    let currentNumber = count;
+
+    console.log(data);
+    for(let i = 0; i < repeatCount; i++) {
+        for(let j = 0; j < count; j++) {
+            if(data[j]["id"] == currentNumber) {
+                nameVar = data[j]["name"];
+                textVar = data[j]["contents"];
+                contents += '<tr><td class="tableelement">${nameVar}</td><td class="tableelement">${textVar}</td></tr>';
+                console.log(contents);
+                currentNumber-=1;
+                articleHolder.innerHTML = contents;
+                break;
+            }
+        }
+    }    
+}
+
+retrieveFeedbackLogs();
