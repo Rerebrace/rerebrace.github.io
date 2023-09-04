bb8c99402687d661628ea9593bfa1a411dd96bff
Hyqerion199 <matjel08@gmail.com>
2023-09-04 13:56:16 -0400

    here added prevention and 2.0

--- games/tower/index.js
+++ games/tower/index.js
@@ -8,57 +8,70 @@ let tablename = "Download Numbers";
 let idone = "download1.1"
 let idtwo = "download1.2"
 let idthree = "download1.3"
+let idfour = "download2.0"
 let rowname = "downloads"
 
 async function updateDownloads(whichdoc) {
+    if (localStorage.getItem("downlaoded") === null) {
+        const { data, error } = await _supabase.from(tablename).select()
 
-    const { data, error } = await _supabase.from(tablename).select()
-
-    let thisvar = document.getElementById(whichdoc);
-    if (whichdoc == "11downloads") {
-        for (let i = 0; i < 3; i++) {
-            if (data[i]['id'] === idone) {
-                console.log(data[i])
-                let newhere = data[i][rowname] +1
-                const { error1 } = await _supabase.from(tablename).update({ downloads: newhere }).eq('id', idone)
-                thisvar.innerHTML = `Version 1.1 - ${newhere} Downloads`;
+        let thisvar = document.getElementById(whichdoc);
+        if (whichdoc == "11downloads") {
+            for (let i = 0; i < 4; i++) {
+                if (data[i]['id'] === idone) {
+                    console.log(data[i])
+                    let newhere = data[i][rowname] + 1
+                    const { error1 } = await _supabase.from(tablename).update({ downloads: newhere }).eq('id', idone)
+                    thisvar.innerHTML = `Version 1.1 - ${newhere} Downloads`;
+                }
             }
+            localStorage.setItem("downlaoded", "true");
         }
-    }
-    if (whichdoc == "12downloads") {
-        for (let i = 0; i < 3; i++) {
-            if (data[i]['id'] === idtwo) {
-                console.log(data[i])
-                let newhere = data[i][rowname] +1
-                const { error1 } = await _supabase.from(tablename).update({ downloads: newhere }).eq('id', idtwo)
-                thisvar.innerHTML = `Version 1.2 - ${newhere} Downloads`;
+        if (whichdoc == "12downloads") {
+            for (let i = 0; i < 4; i++) {
+                if (data[i]['id'] === idtwo) {
+                    console.log(data[i])
+                    let newhere = data[i][rowname] + 1
+                    const { error1 } = await _supabase.from(tablename).update({ downloads: newhere }).eq('id', idtwo)
+                    thisvar.innerHTML = `Version 1.2 - ${newhere} Downloads`;
+                }
             }
+            localStorage.setItem("downlaoded", "true");
         }
-    }
-    // Made by Hyqerion199 (github.com/hyqerion199)
-    if (whichdoc == "13downloads") {
-        for (let i = 0; i < 3; i++) {
-            if (data[i]['id'] === idthree) {
-                console.log(data[i])
-                let newhere = data[i][rowname] +1
-                const { error1 } = await _supabase.from(tablename).update({ downloads: newhere }).eq('id', idthree)
-                thisvar.innerHTML = `Version 1.3 - ${newhere} Downloads`;
+        // Made by Hyqerion199 (github.com/hyqerion199)
+        if (whichdoc == "13downloads") {
+            for (let i = 0; i < 4; i++) {
+                if (data[i]['id'] === idthree) {
+                    console.log(data[i])
+                    let newhere = data[i][rowname] + 1
+                    const { error1 } = await _supabase.from(tablename).update({ downloads: newhere }).eq('id', idthree)
+                    thisvar.innerHTML = `Version 1.3 - ${newhere} Downloads`;
+                }
             }
+            localStorage.setItem("downlaoded", "true");
         }
-    }
-    if (whichdoc == "20downloads") {
-        thisvar.innerHTML = 'Version 2.0';// - 0 Downloads';
+        if (whichdoc == "20downloads") {
+            for (let i = 0; i < 4; i++) {
+                if (data[i]['id'] === idfour) {
+                    console.log(data[i])
+                    let newhere = data[i][rowname] + 1
+                    const { error1 } = await _supabase.from(tablename).update({ downloads: newhere }).eq('id', idfour)
+                    thisvar.innerHTML = `Version 2.0 - ${newhere} Downloads`;
+                }
+            }
+            localStorage.setItem("downlaoded", "true");
+        }
     }
 }
 
 async function firstload() {
-    twozero.innerHTML = 'Version 2.0';// - 0 Downloads';
+    twozero.innerHTML += 'Version 2.0 - 0 Downloads';
     onetwo.innerHTML += `Version 1.2 - 0 Downloads`;
     oneone.innerHTML += `Version 1.1 - 0 Downloads`;
     onethree.innerHTML += `Version 1.3 - 0 Downloads`;
 
     const { data, error } = await _supabase.from(tablename).select('')
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 4; i++) {
         if (data[i]['id'] === idone) {
             console.log(data[i])
             let newhere = data[i][rowname]
@@ -73,6 +86,11 @@ async function firstload() {
             console.log(data[i])
             let newhere = data[i][rowname]
             onethree.innerHTML = `Version 1.3 - ${newhere} Downloads`;
+        }
+        if (data[i]['id'] === idfour) {
+            console.log(data[i])
+            let newhere = data[i][rowname]
+            twozero.innerHTML = `Version 2.0 - ${newhere} Downloads`;
         }
     }
 }
