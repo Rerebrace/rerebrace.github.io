0fbda14baa7c4ed1b878a190e7172ae39b8b7327
Rerebrace <125512517+Rerebrace@users.noreply.github.com>
2024-05-27 19:54:22 -0400

    Create index.js

---
+++ games/minesweeper/index.js
@@ -0,0 +1,44 @@
+// Made by Hyqerion199 (github.com/hyqerion199)
+
+let onezero = document.getElementById("10downloads");
+let tablename = "Download Numbers Minesweeper";
+let idone = "download1.0"
+let rowname = "downloads"
+
+async function updateDownloads(whichdoc) {
+    if (localStorage.getItem("downlaoded") === null) {
+        const { data, error } = await _supabase.from(tablename).select()
+
+        let thisvar = document.getElementById(whichdoc);
+        if (whichdoc == "10downloads") {
+            for (let i = 0; i < 1; i++) {
+                if (data[i]['id'] === idone) {
+                    console.log(data[i])
+                    let newhere = data[i][rowname] + 1
+                    const { error1 } = await _supabase.from(tablename).update({ downloads: newhere }).eq('id', idone)
+                    thisvar.innerHTML = `Version 1.0 - ${newhere} Downloads`;
+                }
+            }
+            localStorage.setItem("downlaoded", "true");
+        }
+        }
+    }
+}
+
+async function firstload() {
+    oneone.innerHTML += `Version 1.0 - 0 Downloads`;
+
+    const { data, error } = await _supabase.from(tablename).select('')
+    for (let i = 0; i < 1; i++) {
+        if (data[i]['id'] === idone) {
+            console.log(data[i])
+            let newhere = data[i][rowname]
+            oneone.innerHTML = `Version 1.0 - ${newhere} Downloads`;
+        }
+
+    }
+}
+
+
+
+firstload();
